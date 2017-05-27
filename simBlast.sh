8817ffb72ef0956f6ece01a0f772982605d90728
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 16:32:22 -0400

    new

--- simBlast.sh
+++ simBlast.sh
@@ -1,4 +1,8 @@
-python makeseed.py
+working_dir =  $(date|sed 's/ /_/g')
 
+mkdir $working_dir
 
+cd $working_dir
+python ../makeseed.py
 /media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f seed.fa -n 4 -o Phy -b 0.32
+cd ..
