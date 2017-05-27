4d45d5c2a1337aa5c4390edc8e791c66446b41aa
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 16:28:04 -0400

    new

--- simBlast.sh
+++ simBlast.sh
@@ -1,5 +1,4 @@
-python MakeSeed.py
-for f in *fa
-do
-	/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f $f -n 4 -o Phy -b 0.32
-done
+python makeseed.py
+
+
+/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f seed.fa -n 4 -o Phy -b 0.32
