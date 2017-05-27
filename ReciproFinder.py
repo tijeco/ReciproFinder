b9728321a8bb8387e77d7574eebe9eae742da6bd
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 14:45:21 -0400

    it works!

--- ReciproFinder.py
+++ ReciproFinder.py
@@ -1,27 +1,29 @@
 pairDict = {}
-with open("sampleBlast.txt") as f:
+with open("Sim_genomes.fasta.blastall") as f:
     for line in f:
         row = line.strip().split()
-        bit_score = int(row[2])
-        taxa1 = row[0].split('_')[0]
+        bit_score = float(row[11])
+        gene1 = row[0]
+        gene2 = row[1]
+        taxa1 = gene1.split('_')[0]
         taxa2 = row[1].split('_')[0]
-
-        # print(row[0] == row[1])
         if taxa1 != taxa2:
             if taxa1 not in pairDict:
                 pairDict[taxa1] = {}
-                pairDict[taxa1][row[0]] =  (row[1],bit_score)
-            else:
-                try:
-                    if pairDict[taxa1][row[0]][1] < bit_score:
-                        pairDict[taxa1][row[0]] =  (row[1],bit_score)
-                except:
-                    pairDict[taxa1][row[0]] =  (row[1],bit_score)
-    print(pairDict)
-    for i in pairDict.keys():
-        for j in pairDict[i].keys():
-            bestie =pairDict[i][j][0]
-            bestieTaxa = pairDict[i][j][0].split("_")[0]
-            bestieOfBestie = pairDict[bestieTaxa][bestie][0]
-            if(j == bestieOfBestie):
-                print(j," and ", bestie," are BFFs")
+            if gene1 not in pairDict[taxa1]:
+                pairDict[taxa1][gene1] = {}
+            if taxa2 not in pairDict[taxa1][gene1]:
+                pairDict[taxa1][gene1][taxa2] = (gene2, bit_score)
+            elif pairDict[taxa1][gene1][taxa2][1] < bit_score:
+                pairDict[taxa1][gene1][taxa2] = (gene2, bit_score)
+    bestieDict = {}
+    for taxa in pairDict.keys():
+        for gene in pairDict[taxa].keys():
+            for genePairTaxa in pairDict[taxa][gene]:
+                bestie =  pairDict[taxa][gene][genePairTaxa][0]
+                bestieOfBestie = pairDict[genePairTaxa][bestie][taxa][0]
+                if gene == bestieOfBestie:
+                    if min(bestie,gene) not in bestieDict:
+                        bestieDict[min(bestie,gene)] = max(bestie,gene)
+                    else:
+                        print(bestie, "and",gene,"are BFFs")
