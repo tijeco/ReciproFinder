abb29cd1946a9767e6068ac890a4a1a6bdc5aa3e
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 17:31:14 -0400

    options

--- simBlast.sh
+++ simBlast.sh
@@ -5,14 +5,14 @@ mkdir $working_dir
 
 cd $working_dir
 python ../makeseed.py
-/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f seed.fa -n 4 -o Phy -b 0.32
+/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f seed.fa -n $1 -o Phy -b 0.32
 python3 ../splitTogene.py
 
 for f in gene*
 
 do
 	cd $f
-	/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f $f.fa -n 4 -o Phy -b 0.32
+	/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f $f.fa -n $1 -o Phy -b 0.32
 	cd ..
 
 
