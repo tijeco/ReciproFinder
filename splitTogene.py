414b2c01f0b8fda2ac7db8e455e89cfe18b3f8b9
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 16:46:12 -0400

    line

--- splitTogene.py
+++ splitTogene.py
@@ -1,8 +1,9 @@
 number = 1
 with open("seed_Unaligned.FASTA") as f:
-    if line[0] != ">":
-        with open("gene"+str(number)+".fa","w") as out:
-            out.write(line)
-        
+    for line in f:
+        if line[0] != ">":
+            with open("gene"+str(number)+".fa","w") as out:
+                out.write(line)
+            
 
-    number+=1
+        number+=1
