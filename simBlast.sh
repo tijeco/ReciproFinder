a39d76bf2cb7d5532ceb12410902e64f8a162a53
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 16:53:47 -0400

    py3s

--- simBlast.sh
+++ simBlast.sh
@@ -1,3 +1,4 @@
+
 working_dir=$(date|sed 's/ /_/g')
 echo $working_dir
 mkdir $working_dir
@@ -5,5 +6,11 @@ mkdir $working_dir
 cd $working_dir
 python ../makeseed.py
 /media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f seed.fa -n 4 -o Phy -b 0.32
-python ../splitTogene.py
+python3 ../splitTogene.py
+for f in *fa
+do
+
+mkdir $f
+
+done
 cd ..
