f5c9463632b9351e62e398bdfaff50afee874d3c
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 16:53:12 -0400

    new

--- splitTogene.py
+++ splitTogene.py
@@ -1,8 +1,12 @@
+import os
+
 number = 1
 with open("seed_Unaligned.FASTA") as f:
     for line in f:
         if line[0] != ">":
-            with open("gene"+str(number)+".fa","w") as out:
+            filename = "gene"+str(number)+"/gene"+str(number)+".fa"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename,"w") as out:
                 out.write(line)
 
 
