c0cf557eb9561064305c54bfed0f402ae0599d62
Jeff Cole <coleti16@students.ecu.edu>
2017-05-27 16:23:42 -0400

    evolveagene

--- simBlast.sh
+++ simBlast.sh
@@ -1,9 +1,5 @@
 python MakeSeed.py
-
-/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f gene4.fa -n 4 -o Phy -b 0.32
-
-for i in $1
+for f in *fa
 do
-	mkdir gene_$1
-
+	/media/BigRAID/JeffTemp/EvolvAGene4Package/EvolveAGene4-linux-x86-64 -f $f -n 4 -o Phy -b 0.32
 done
